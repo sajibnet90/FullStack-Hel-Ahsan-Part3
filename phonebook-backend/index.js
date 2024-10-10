452ea57332e39768c60890011d40b6acbd68e86e
Ahsan <sajibnet@gmail.com>
2024-10-11 01:19:00 +0300

    Add Phonebook part3 Ex 3.15 -3.18

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -8,9 +8,37 @@ const app = express();
 //serving the static frontend build
 app.use(express.static('dist'))
 
+
 app.use(express.json());
 app.use(cors());
 
+//---------------------custom middlewares-----------------------------------------
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+  
+  app.use(requestLogger)//using request logger middleware
+  
+  //-------
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+    next(error)
+  }
+  
+  //------
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  
 // let persons = [
 //     { id: "1", name: "Arto Hellas", number: "040-123456" },
 //     { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
@@ -76,10 +104,16 @@ app.get('/info', (request, response) => {
 
 //##################### GET by id ###############
 
-app.get('/api/notes/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+        if(person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
     })
+    .catch(error => next(error))
   })
 
 // app.get('/api/persons/:id', (req, res) => {
@@ -94,18 +128,48 @@ app.get('/api/notes/:id', (request, response) => {
 // });
 
 //##################### DELETE by id ###################
-
-app.delete('/api/persons/:id', (req, res) => {
-    const id = req.params.id;
-    const index = persons.findIndex(p => p.id === id);
-
-    if (index !== -1) {
-        persons.splice(index, 1);
-        res.status(204).end(); // 204 No Content
-    } else {
-        res.status(404).send({ error: 'Person not found' });
-    }
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        if (result) {
+          response.status(200).json({ message: `Person with id ${request.params.id} was successfully deleted` });
+        } else {
+          response.status(404).send({ error: 'Person not found' });
+        }
+      })
+      .catch(error => next(error)); // Pass errors to error handler
 });
+
+
+// app.delete('/api/persons/:id', (req, res) => {
+//     const id = req.params.id;
+//     const index = persons.findIndex(p => p.id === id);
+
+//     if (index !== -1) {
+//         persons.splice(index, 1);
+//         res.status(204).end(); // 204 No Content
+//     } else {
+//         res.status(404).send({ error: 'Person not found' });
+//     }
+// });
+
+//######################### PUT update #########################################################
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body;
+    if (!name || !number) {
+      return response.status(400).json({ error: 'name or number missing' });
+    }
+    const updatedPerson = { name, number };
+  
+    // Options to return the updated document
+    Person.findByIdAndUpdate(request.params.id,updatedPerson,{ new: true})
+      .then(updatedPerson => {
+          response.json(updatedPerson);
+      })
+      .catch(error => next(error));
+  });
+  
 
 //#################### POST to Mongo###############
 
@@ -149,6 +213,10 @@ app.post('/api/persons', (request, response) => {
 //     res.json(newPerson);
 // });
 
+
+
+app.use(errorHandler)//error handling middleware
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
